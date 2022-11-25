24871012167b87ada8080ad860599e28a4069870
Guillermo Croppi <guillermocroppi@gmail.com>
2022-11-25 19:04:59 -0300

    activacion now supports links (not done)

--- models/timeline.js
+++ models/timeline.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const mongoosePaginate = require('mongoose-paginate')
 
+const LinksSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  title: { type: String, required: true }
+})
 
 // Define `Timeline` Schema
 const Timeline = new mongoose.Schema({
@@ -9,7 +13,8 @@ const Timeline = new mongoose.Schema({
   text: { type: String },
   date: { type: Date, required: true },
   youtubeId: { type: String },
-  imageUrl: { type: String }
+  imageUrl: { type: String },
+  links: [LinksSchema]
 })
 
 // Model's Plugin Extensions
